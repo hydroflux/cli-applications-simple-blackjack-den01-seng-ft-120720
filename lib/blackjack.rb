f6b213b14eb0d2a9cf6bfa03a9c912a27efc5f4c
Jack Hubert <jphubert93@gmail.com>
2020-12-05 23:55:56 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,52 @@
+require 'pry'
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{cart_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp.downcase
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card_total = deal_card + deal_card
+  display_card_total(card_total)
+  card_total
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+  prompt_user
+  user_input = get_user_input 
+  while user_input != 'h' || user_input != 's' do
+    invalid_command
+    puts user_input
+    user_input = get_user_input
+  end
+  if get_user_input == 'h'
+    card_total += deal_card
+  else
+    card_total
+  end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +54,11 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  card_total = hit?(initial_round)
+  while card_total <= 21 do
+    hit?(card_total)
+  end
+  end_game
 end
     
