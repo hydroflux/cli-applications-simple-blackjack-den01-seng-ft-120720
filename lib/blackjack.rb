d239a57b19c8d6f17367c3f6ed10dc329e365007
Jack Hubert <jphubert93@gmail.com>
2020-12-06 00:11:51 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -71,8 +71,6 @@ end
 def runner
   welcome
   card_total = initial_round
-  card_total = hit?(initial_round)
-  display_card_total(card_total)
   while card_total <= 21 do
     hit?(card_total)
     display_card_total(card_total)
