93137d33fad7bc6dd34ef8374aafb50548f91079
Jack Hubert <jphubert93@gmail.com>
2020-12-06 00:01:36 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,7 +35,6 @@ def hit?(card_total)
   user_input = get_user_input 
   while user_input != 'h' && user_input != 's' do
     invalid_command
-    puts user_input
     user_input = get_user_input
   end
   if get_user_input == 'h'
